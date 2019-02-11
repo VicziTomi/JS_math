40f47454468b02a4f760aaa920731e43074a40b4
VicziTomi <viczian.t@gmail.com>
2019-02-11 16:27:28 +0100

    sose lesz kesz

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-/* const power = require('./pow'); */
-const fadtorial = require('./fact.js');
+const power = require('./pow');
+const factorial = require('./fact.js');
 let readline = require('readline-sync');
 
 function randomNumber (number) {
-  number = Math.floor((Math.random() * 8) + 1);
+  let number = Math.floor((Math.random() * 8) + 1);
 }
 let szam = [1,2,3,4,5,6,7,8];
 let index =readline.keyInSelect(szam, "Melyik szam legyen?");
+
+console.log('Hatvany: ', power.pow);
+console.log('Faktorialis: ', factorial.fact(randomNumber));
 
